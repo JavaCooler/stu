9603c6e0d14564eb14ddfdf107b11e6a017e5b53
JavaCooler <838969025@qq.com>
2018-12-18 14:56:34 +0800

    01

---
+++ Eureka/src/main/java/com/tedu/RunAppEureka.java
@@ -0,0 +1,16 @@
+package com.tedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer	//标识这是一个EurekaServer程序
+public class RunAppEureka {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunAppEureka.class, args);
+	}
+
+}
